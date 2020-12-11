e647539c3e6e8c11d913ca6821864407d0ea101d
Amogh Karhadkar <amogh.cs18@bmsce.ac.in>
2020-12-11 19:18:20 +0530

    Fixed bugs

--- controllers/process.js
+++ controllers/process.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("../middleware/async");
 const Process = require("../models/Process");
+const CompanyYear = require("../models/CompanyYears");
 
 // @desc        get a process, given company name and a year
 // @route       GET  /api/v1/process
@@ -16,6 +17,18 @@ exports.getProcess = asyncHandler(async (req, res, next) => {
 // @route       POST  /api/v1/process
 // @access      recruiter only
 exports.createProcess = asyncHandler(async (req, res, next) => {
+  const company = await CompanyYear.findOne({ company: req.body.name });
+  if (company) {
+    await CompanyYear.update(
+      { company: req.body.name },
+      { years: [...company.years, req.body.year] }
+    );
+  } else {
+    await CompanyYear.create({
+      company: req.body.name,
+      years: [req.body.year],
+    });
+  }
     const process = await Process.create(req.body);
     res.status(201).json({
         success: true,
